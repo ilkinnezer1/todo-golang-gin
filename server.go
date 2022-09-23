37a3a4d3d07434ce83d2acfccc06e8dae3252fa8
ilkinnezer1 <90461640+ilkinnezer1@users.noreply.github.com>
2022-09-24 01:40:01 +0300

    Initial addition - Get Request completed

---
+++ server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// Elements of Todo app
+type todoElements struct {
+	Id          int    `json:"Id"`
+	Task        string `json:"Task"`
+	isCompleted bool   `json:"Completed"`
+}
+
+var todoTasks = []todoElements{
+	{Id: 1, Task: "Sleep", isCompleted: true},
+	{Id: 2, Task: "Read", isCompleted: true},
+	{Id: 3, Task: "Code", isCompleted: true},
+}
+
+// Handle Error
+func handleError(err error) {
+	if err != nil {
+		fmt.Println("Oops, there is an error: ", err)
+	}
+}
+
+// Get The tasks
+func getTasks(context *gin.Context) {
+	//Json conversion
+	context.IndentedJSON(http.StatusOK, todoTasks)
+}
+
+func main() {
+	// Run the server
+	server := gin.Default()
+	err := server.Run("localhost:5050")
+	handleError(err)
+
+	server.GET("/todoTasks")
+}
