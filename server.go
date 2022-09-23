064e8997b950e8f0fab14107a5ca33471918c112
ilkinnezer1 <90461640+ilkinnezer1@users.noreply.github.com>
2022-09-24 02:36:15 +0300

    - Get Request for singe task completed and tested on postman

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -52,11 +53,30 @@ func addTasks(context *gin.Context) {
 	}
 }
 
+func getSingleTask(id int) (*todoElements, error) {
+	for index, task := range todoTasks {
+		if task.Id == id {
+			return &todoTasks[index], nil
+		}
+	}
+	return nil, errors.New("task doesn't exist")
+}
+func getTask(context *gin.Context) {
+	taskID, _ := strconv.Atoi(context.Param("id")) // That convert string id to the int
+	task, err := getSingleTask(taskID)
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Task doesn't exits"})
+	}
+	context.IndentedJSON(http.StatusOK, task)
+
+}
+
 func main() {
 	// Run the server
 	server := gin.Default()
 	server.GET("/tasks", getTasks)
 	server.POST("/add-task", addTasks)
+	server.GET("/tasks/:id", getTask)
 
 	err := server.Run("localhost:5050")
 	handleError(err)
